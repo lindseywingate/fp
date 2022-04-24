3bba51463a899b07b1792bff39436181d7fc75cb
darthvader1118 <kyellayi@gmail.com>
2022-04-23 23:55:02 -0400

    Found how to get amplitude working on filtering commercials

--- org/wikijava/sound/playWave/PlaySound.java
+++ org/wikijava/sound/playWave/PlaySound.java
@@ -3,6 +3,7 @@ package org.wikijava.sound.playWave;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -11,10 +12,12 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.DataLine.Info;
+
+
 /**
- *
+ * 
  * <Replace this with a short description of the class.>
- *
+ * 
  * @author Giulio
  */
 public class PlaySound {
@@ -27,73 +30,75 @@ public class PlaySound {
      * CONSTRUCTOR
      */
     public PlaySound(InputStream waveStream) {
-        this.waveStream = waveStream;
+	this.waveStream = waveStream;
     }
 
     public void play() throws PlayWaveException {
 
-        AudioInputStream audioInputStream = null;
-        try {
-            //audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
+	AudioInputStream audioInputStream = null;
+	try {
+	    //audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
+		
+		//add buffer for mark/reset support, modified by Jian
+		InputStream bufferedIn = new BufferedInputStream(this.waveStream);
+	    audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
+		
+	} catch (UnsupportedAudioFileException e1) {
+	    throw new PlayWaveException(e1);
+	} catch (IOException e1) {
+	    throw new PlayWaveException(e1);
+	}
 
-            //add buffer for mark/reset support, modified by Jian
-            InputStream bufferedIn = new BufferedInputStream(this.waveStream);
-            audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
+	// Obtain the information about the AudioInputStream
+	AudioFormat audioFormat = audioInputStream.getFormat();
+	Info info = new Info(SourceDataLine.class, audioFormat);
 
-        } catch (UnsupportedAudioFileException e1) {
-            throw new PlayWaveException(e1);
-        } catch (IOException e1) {
-            throw new PlayWaveException(e1);
-        }
+	// opens the audio channel
+	SourceDataLine dataLine = null;
+	try {
+	    dataLine = (SourceDataLine) AudioSystem.getLine(info);
+	    dataLine.open(audioFormat, this.EXTERNAL_BUFFER_SIZE);
+	} catch (LineUnavailableException e1) {
+	    throw new PlayWaveException(e1);
+	}
 
-        // Obtain the information about the AudioInputStream
-        AudioFormat audioFormat = audioInputStream.getFormat();
-        Info info = new Info(SourceDataLine.class, audioFormat);
-        System.out.println("sound data: "+audioFormat.getSampleRate());
-        //48kHz, 1 channel, 16 sample size, 2 frame size, 48kHz frame rate
+	// Starts the music :P
+	dataLine.start();
 
-        // opens the audio channel
-        SourceDataLine dataLine = null;
-        try {
-            dataLine = (SourceDataLine) AudioSystem.getLine(info);
-            dataLine.open(audioFormat, this.EXTERNAL_BUFFER_SIZE);
-        } catch (LineUnavailableException e1) {
-            throw new PlayWaveException(e1);
-        }
+	int readBytes = 0;
+	byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
 
-        // Starts the music :P
-        dataLine.start();
-
-        int readBytes = 0;
-        byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
-
-        try {
-            while (readBytes != -1) {
-                byte largest = 0;
-                for(int n = 0; n<audioBuffer.length; n++) {
-                    System.out.println("BYTE "+audioBuffer[n]);
-                    if(audioBuffer[n] > largest) {
-                        largest = audioBuffer[n];
-                    }
-                }
-                System.out.println("largest "+largest);
-
-                readBytes = audioInputStream.read(audioBuffer, 0,
-                        audioBuffer.length);
-                System.out.println("microseconds "+dataLine.getMicrosecondPosition());
-                System.out.println("frame position "+dataLine.getFramePosition());
-                System.out.println("get level "+ dataLine.getLevel());
-                if (readBytes >= 0){
-                    dataLine.write(audioBuffer, 0, readBytes);
-                }
-            }
-        } catch (IOException e1) {
-            throw new PlayWaveException(e1);
-        } finally {
-            // plays what's left and and closes the audioChannel
-            dataLine.drain();
-            dataLine.close();
-        }
+	try {
+	    while (readBytes != -1) {
+		readBytes = audioInputStream.read(audioBuffer, 0,
+			audioBuffer.length);
+			double amplitude = 0;
+			for (int i = 0; i < audioBuffer.length/2; i++) {
+				double y = (audioBuffer[i*2] | audioBuffer[i*2+1] << 8) / 32768.0;
+				// depending on your endianness:
+				//double y = (audioBuffer[i*2]<<8 | audioBuffer[i*2+1]) / 32768.0;
+				amplitude += Math.abs(y);
+			}
+			amplitude = amplitude / audioBuffer.length / 2;
+			System.out.println(amplitude);
+		if (readBytes >= 0){
+			byte[] clone = audioBuffer.clone();
+			//Arrays.sort(clone);
+			System.out.println(audioBuffer[clone.length-1]);
+			byte temp = clone[0];
+			byte  end = clone[clone.length-1];
+			clone[clone.length-1] = temp;
+			clone[0] = end;
+		    dataLine.write(audioBuffer, 0, readBytes);
+		}
+	    }
+	} catch (IOException e1) {
+	    throw new PlayWaveException(e1);
+	} finally {
+	    // plays what's left and and closes the audioChannel
+	    dataLine.drain();
+	    dataLine.close();
+	}
 
     }
 }
